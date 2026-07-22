dfe569233ba346fa9f04bbca7ad6cdd0a78b315e
Uzair Khan <khanuzair54670@gmail.com>
2026-07-22 22:49:42 +0530

    Add explicit headless: true to Playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -39,6 +39,7 @@ export default defineConfig({
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    headless: true,
     screenshot: 'only-on-failure',
     video: 'retain-on-failure',
     trace: 'retain-on-failure',
