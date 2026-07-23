fa327b621e1087c4db419fe0e37ae1afe9cbee5e
Uzair Khan <khanuzair54670@gmail.com>
2026-07-23 22:53:27 +0530

    Enable parallel execution in Playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -20,13 +20,13 @@ dotenv.config({
 export default defineConfig({
   testDir: './tests',
   /* Run tests in files in parallel */
-  fullyParallel: false,
+  fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: process.env.CI ? 1 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 3 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [['html', { open: 'always' }], ['junit', { outputFile: 'test-results/junit-report.xml'}]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
