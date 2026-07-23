b7d52e65af9a0d89d2d15caa385c8c053e8e8dba
Uzair Khan <khanuzair54670@gmail.com>
2026-07-23 21:49:21 +0530

    Added Junit reporter

--- playwright.config.ts
+++ playwright.config.ts
@@ -28,7 +28,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [['html', { open: 'always' }]],
+  reporter: [['html', { open: 'always' }], ['junit', { outputFile: 'test-results/junit-report.xml'}]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   timeout: 90000,
   expect: {
